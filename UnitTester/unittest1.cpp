a3e81acff26312d0503c0a4af0936fee62f3fce7
Zeus <kyle.b.amonson@hotmail.com>
2014-09-22 16:13:59 -0700

    Test now builds

--- UnitTester/unittest1.cpp
+++ UnitTester/unittest1.cpp
@@ -1,18 +1,26 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "readint.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTester
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(READINT_TEST) // Test suite for read_int()
 	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			// TODO: Your test code here
-		}
 
+		TEST_METHOD(TestCase2) // Test case 2: for invalid argument
+		{
+			// define a C++11 Lambda function to be called by your test
+			auto func = []() {
+				// call with incorrect arguments (test case 2)
+				read_int("My prompt: ", 5, 1);
+			};
+
+			// We expect an invalid_argument exception to be thrown when we call func!
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
 	};
+
 }
