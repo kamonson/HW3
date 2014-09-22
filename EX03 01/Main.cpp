569602e37acc9cb6264cd001b8d5820968166020
Zeus <kyle.b.amonson@hotmail.com>
2014-09-22 00:46:58 -0700

    Added exceptions and comments

--- EX03 01/Main.cpp
+++ EX03 01/Main.cpp
@@ -1,8 +1,10 @@
+/*Zeus CS273-Data Structures*/
+
 #include <iostream>
 #include <stdexcept>
 #include "readint.h"
 using namespace std;
-
+/*Ask user for a range of numbers, display the number the user selects if it is within range*/
 int main()
 {
 	int low, high;
@@ -13,10 +15,18 @@ int main()
 	cin >> low;
 	try
 	{
+		/*test to ensure that number range is valid and argument is an int value*/
+//		throw (double) high, low; try to catch double value being entered--did not work
+		if (low >= high) throw invalid_argument("Invalid range, renter low:");
 		int number = read_int("Please enter a number within the range: ", low, high);
 		cout << "You entered " << number << endl;
 	}
+	/*if range is wrong display error and end program*/
 	catch (invalid_argument &e) {
 		cerr << "Exception: You supplied an invalid argument for read_int!\n";
 	}
+//	/*if double value is given*/ attempted but did not work correctly, read all entries as an error including proper ones
+//	catch (double){
+//		cerr << "Value for range must be an integer value.\n";
+//	}
 }
--- EX03 01/readint.cpp
+++ EX03 01/readint.cpp
@@ -8,17 +8,28 @@
 using namespace std;
 
 int read_int(const string &prompt, int low, int high){
-
+	/*user picks number, number returned, 0 returned if code has a problem*/
 	cin.exceptions(ios_base::failbit);
 	int num = 0;
 	while (true) {
 		try{
 			cout << prompt;
 			cin >> num;
+			/*check to ensure that the number entered is between the high and low range*/
+			if (num < low || num > high) {
+				throw out_of_range ("the number you have entered is not within the selected range, please enter a number within the specified range: \n");
+			}
 			return num;
 		}
+		/*return error message and allow user to select new number*/
 		catch (ios_base::failure& ex){
 			cout << "Bad numeric string -- try again\n";
+			cin.clear();
+			cin.ignore(numeric_limits<int>::max(), '\n');
+		}
+		/*return error message and allow user to select new number*/
+		catch (out_of_range){
+			cerr << "Range is between between " << low << " and " << high << ".\n";
 			cin.clear();
 			cin.ignore(numeric_limits<int>::max(), '\n');
 		}
