8c064a1ae3ddb0dec0e5a139a260ec5609077673
Zeus <kyle.b.amonson@hotmail.com>
2014-09-22 15:01:02 -0700

    Adjusted to meet HW3 part 1 expectations, removed extra exeptions from main

--- EX03 01/Main.cpp
+++ EX03 01/Main.cpp
@@ -15,18 +15,14 @@ int main()
 	cin >> low;
 	try
 	{
-		/*test to ensure that number range is valid and argument is an int value*/
-//		throw (double) high, low; try to catch double value being entered--did not work
+		/*check to ensure that number range is valid*/
 		if (low >= high) throw invalid_argument("Invalid range, renter low:");
+		/*Run read_int then out put numeric info*/
 		int number = read_int("Please enter a number within the range: ", low, high);
 		cout << "You entered " << number << endl;
 	}
-	/*if range is wrong display error and end program*/
+	/*if range is invalid display error and end program*/
 	catch (invalid_argument &e) {
 		cerr << "Exception: You supplied an invalid argument for read_int!\n";
 	}
-//	/*if double value is given*/ attempted but did not work correctly, read all entries as an error including proper ones
-//	catch (double){
-//		cerr << "Value for range must be an integer value.\n";
-//	}
 }
