8d41d706c452a9d578bd969796df492a05e77015
Zeus <kyle.b.amonson@hotmail.com>
2014-09-22 22:53:19 -0700

    rewrote code

--- EX03 01/readint.cpp
+++ EX03 01/readint.cpp
@@ -12,13 +12,20 @@ int read_int(const string &prompt, int low, int high){
 	cin.exceptions(ios_base::failbit);
 	int num = 0;
 	while (true) {
-		try{
-			cout << prompt;
-			cin >> num;
-			return num;
+		try{			
+			if (low >= high){ throw invalid_argument("Range is invalid"); }
+				cout << prompt;
+				cin >> num;
+				if (num < low || num > high) throw out_of_range("Please select a value within the range: ");
+				return num;
 		}
 		catch (ios_base::failure& ex){
 			cout << "Bad numeric string -- try again\n";
+			cin.clear();
+			cin.ignore(numeric_limits<int>::max(), '\n');
+		}
+		catch (out_of_range& ex){
+			cerr << "Range is between " << low << " and " << high << endl;
 			cin.clear();
 			cin.ignore(numeric_limits<int>::max(), '\n');
 		}
